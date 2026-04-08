3ae8536e070f2391cdd1dea2847304f1fe67fe32
WilliamCarta <william57650@yahoo.fr>
2026-04-08 19:33:37 +0200

    Fix GET Games by id Add POST Games (try)

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -11,7 +11,7 @@ namespace GameBackLogApi.Controllers
         {
             new() {ID = 1, Title = "Hollow Knight", Genre = "Platformer", Status = "PLAYING"},
             new() {ID = 2, Title = "CS:GO", Genre = "FPS", Status = "DROPPED"},
-            new() {ID = 3, Title = "Cyberpunk 2077", Genre = "RPG", Status = "Wishlist" }
+            new() {ID = 3, Title = "Cyberpunk 2077", Genre = "RPG", Status = "WISHLIST" }
         };
 
         [HttpGet]
@@ -21,21 +21,37 @@ namespace GameBackLogApi.Controllers
         }
 
         //On crée une methode HttpGet pour récupérer le jeu que l'on souhaite en fonction de son id (URL/games/id)
-        //On fait id - 1 car la position dans la liste de l'id 1 = 0
+        //On utilise g => g.ID == id pour que FirstOrDefault nous retourne le jeu qui correspond à l'id, + propre que de chercher via l'index (games[id])
 
         [HttpGet("{id}")]
         public ActionResult<Game> GetByID(int id)
         {
-            id -= 1;
+            var game = games.FirstOrDefault(g => g.ID == id);
             
-            if (id < 0 || id >= games.Count)
+            if (game == null)
             {
                 return NotFound();
             }
 
-            return games[id];
+            return game;
 
         }
+
+        [HttpPost]
+
+        public ActionResult<Game> Add(Game newGame)
+        {
+            int newID = games.Count + 1;
+
+            newGame.ID = newID;
+
+            games.Add(newGame);
+
+            return newGame;
+
+        }
+
+       
 
     }
 }
