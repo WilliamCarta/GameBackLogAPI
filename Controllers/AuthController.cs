62baece4bb6d888498eaad94c5286915d5d26097
WilliamCarta <william57650@yahoo.fr>
2026-04-09 15:50:25 +0200

    Add AuthController with static list

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameBackLogApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class AuthController : ControllerBase
+    {
+
+        private static List<User> users = new List<User>
+        {
+           new() {Username = "Willitest", Password = "12345" },
+           new() {Username = "Willitest2", Password = "testmdp" },
+           new() {Username = "Willitest3", Password = "azerty" }
+        };
+
+        [HttpPost("login")]
+
+        public ActionResult<User> CheckAuth(User user)
+        {
+            var authuser = users.FirstOrDefault(u => u.Username == user.Username);
+
+            if (authuser == null || user.Password != authuser.Password)
+            {
+                return NotFound();
+            }
+            else if (authuser.Password == user.Password)
+            {
+
+            }
+
+
+        }
+    }
+}
